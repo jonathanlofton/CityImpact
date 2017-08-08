844f101dee49933f4b41498a560de120711aecb0
Mallik Begur <mbegur@gmail.com>
2017-08-08 10:46:49 -0700

    Add an action and reducer for it

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 // import configureStore from './src/store/store';
 import Root from './src/root';
+// import { requestAllEvents } from './src/components/actions/eventActions';
 //
 // const store = configureStore();
 // window.store = store;
+// window.requestAllEvents = requestAllEvents;
 
 export default class Entry extends Component {
   render() {
