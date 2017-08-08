e4794616ee48097107e4fe9449d2a3eb2ab26461
Tyler Eakes <teakesprofessional@gmail.com>
2017-08-07 22:44:32 -0700

    yes

--- backend/server/modules/meetups/controller.js
+++ backend/server/modules/meetups/controller.js
@@ -1,11 +1,11 @@
 import Meetup from './model';
 
-export const createMeetup = (req, res) => {
+export const createMeetup = async (req, res) => {
   const { title, description } = req.body;
   const newMeetup = new Meetup({ title, description });
 
   try {
-    return res.status(201).json({ meetup: newMeetup.save() });
+    return res.status(201).json({ meetup: await newMeetup.save() });
   } catch (e) {
     return res.status(e.status).json({ error: true, message: 'Error with Meetup' });
   }
