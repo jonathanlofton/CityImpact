fefe75556bf0b7347a17cac35ad83ff2d1ce3044
Mallik Begur <mbegur@gmail.com>
2017-08-10 10:16:36 -0700

    Update state shape for events

--- src/reducers/EventReducer.js
+++ src/reducers/EventReducer.js
@@ -5,19 +5,19 @@ import {
   RECEIVE_SINGLE_EVENT
 } from '../actions/eventActions';
 
-// const defaultState = Object.freeze({
-//   entities: {},
-//   currentEvent: null,
-//   errors: []
-// });
+const defaultState = Object.freeze({
+  entities: {},
+  currentEvent: null,
+  errors: []
+});
 
-const EventReducer = (state = {}, action) => {
+const EventReducer = (state = defaultState, action) => {
   Object.freeze(state);
   let newState;
   switch(action.type) {
     case RECEIVE_ALL_EVENTS:
-      return action.events;
-      // return merge({}, state, { entities: events, errors: []});
+      // return action.events;
+      return merge({}, state, { entities: action.events, errors: []});
     case RECEIVE_SINGLE_EVENT:
       const event = action.event;
       newState = merge({}, state);
